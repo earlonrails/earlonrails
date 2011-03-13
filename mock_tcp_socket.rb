56cad6cd17532e384d823c351f26f7c152c591c9
Kevin Krauss <earlkrauss@gmail.com>
2011-03-13 13:03:56 -0700

    mock a tcp connection for rspec

---
+++ mock_tcp_socket.rb
@@ -0,0 +1,38 @@
+require 'spec/mocks'
+
+TCP_NEW = TCPSocket.method(:new) unless defined? TCP_NEW
+
+#
+# Example:
+#   mock_tcp_next_request("<xml>junk</xml>")
+#
+class FakeTCPSocket
+
+  def readline(some_text = nil)
+    return @canned_response    
+  end
+
+  def flush
+  end
+
+  def write(some_text = nil)
+  end
+
+  def set_canned(response)
+     @canned_response = response
+  end
+
+end
+
+def mock_tcp_next_request(string) 
+  TCPSocket.stub!(:new).and_return {
+    cm = FakeTCPSocket.new
+    cm.set_canned(string)
+    cm
+  }
+end
+
+
+def unmock_tcp
+  TCPSocket.stub!(:new).and_return { TCP_NEW.call }
+end
